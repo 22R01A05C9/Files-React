1c43a559e296fe695e725e8db51c9f168e97ad71
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-03-11 22:22:55 +0530

    added code input in download

---
+++ src/components/cinput.jsx
@@ -0,0 +1,44 @@
+function Cinput(){
+    const inp = (e)=>{
+        document.querySelector(".download .submit button").disabled = true
+        let value = e.target.value.trim()
+        let length = value.length
+        if(length == 0) return;
+        if(length > 1){
+            e.target.value = value.slice(length-1,length)
+        }
+        let next = e.target.nextSibling
+        if(next){
+            e.target.disabled = true
+            next.disabled = false
+            next.focus()
+        }else{
+            document.querySelector(".download .submit button").disabled = false
+        }
+    }
+    const kdown = (e)=>{
+        if(e.key == "Backspace"){
+            document.querySelector(".download .submit button").disabled = true
+            if(e.target.value){
+                e.target.value="";
+                return;
+            }
+            let prev = e.target.previousSibling
+            if(prev){
+                e.target.disabled = true
+                prev.disabled = false
+                prev.focus()
+            }
+        }
+    }
+    return(
+        <div className="dinput">
+            <input type="number" name="i1" onInput={inp} onKeyDown={kdown}/>
+            <input type="number" name="i2" onInput={inp} onKeyDown={kdown} disabled/>
+            <input type="number" name="i3" onInput={inp} onKeyDown={kdown} disabled/>
+            <input type="number" name="i4" onInput={inp} onKeyDown={kdown} disabled/>
+        </div>
+    )
+}
+
+export default Cinput
--- src/components/download/download.jsx
+++ src/components/download/download.jsx
@@ -1,18 +1,13 @@
 import "./download.css"
-
+import Cinput from "../cinput"
 function Download(){
     return(
         <div className="download">
             <h3>Download</h3>
             <p>Enter Code To Download File</p>
-            <div className="dinput">
-                <input type="number" />
-                <input type="number" />
-                <input type="number" />
-                <input type="number" />
-            </div>
+            <Cinput />
             <div className="submit">
-                <button>Submit</button>
+                <button disabled>Submit</button>
             </div>
         </div>
     )

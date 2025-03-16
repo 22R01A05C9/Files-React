77046f00809aa767496cca4974d4ade273781bf6
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-03-16 20:53:33 +0530

    completed download

--- src/components/download/cinput.jsx
+++ src/components/download/cinput.jsx
@@ -1,29 +1,29 @@
-import { useRef } from "react";
 import Toast from "../../helpers/toast";
-function Cinput({submit, buttonref, inputref, file}){
-    const paste = (e)=>{
-        if(file){
+import React from "react";
+const Cinput = React.memo(({ submit, buttonref, inputref, file }) => {
+    const paste = (e) => {
+        if (file) {
             e.preventDefault()
             return;
         }
         let data = e.clipboardData.getData("text");
         let exp = /^[0-9]{4}$/;
-        if(exp.test(data)){
-            inputref.current.forEach((input, index)=>{
-                input.value = data[index-1]
-                if(index <= 3){
+        if (exp.test(data)) {
+            inputref.current.forEach((input, index) => {
+                input.value = data[index - 1]
+                if (index <= 3) {
                     input.disabled = true
-                    inputref.current[index+1].disabled = false
-                    inputref.current[index+1].focus()
+                    inputref.current[index + 1].disabled = false
+                    inputref.current[index + 1].focus()
                 }
             })
-        }else{
-            Toast("Pasting Invalid Code","warn",localStorage.getItem("theme") || "dark")
+        } else {
+            Toast("Pasting Invalid Code", "warn", localStorage.getItem("theme") || "dark")
             e.preventDefault()
-        }        
+        }
     }
-    const inp = (e)=>{
-        if(file){
+    const inp = (e) => {
+        if (file) {
             e.target.value = ""
             e.target.disabled = true
             Toast("File Is Being Downloaded!", "warn", localStorage.getItem("theme") || "dark")
@@ -33,52 +33,52 @@ function Cinput({submit, buttonref, inputref, file}){
         buttonref.current.disabled = true
         let value = e.target.value.trim()
         let length = value.length
-        if(length == 0) return;
-        if(length > 1){
-            e.target.value = value.slice(length-1,length)
+        if (length == 0) return;
+        if (length > 1) {
+            e.target.value = value.slice(length - 1, length)
         }
         let next = e.target.nextSibling
-        if(next){
+        if (next) {
             e.target.disabled = true
             next.disabled = false
             next.focus()
-        }else{
+        } else {
             buttonref.current.removeAttribute("disabled")
         }
     }
-    const kdown = (e)=>{
-        if(file){
+    const kdown = (e) => {
+        if (file) {
             e.target.value = ""
             e.target.disabled = true
             Toast("File Is Being Downloaded!", "warn", localStorage.getItem("theme") || "dark")
             e.preventDefault()
             return;
         }
-        if(e.key == "Backspace"){
+        if (e.key == "Backspace") {
             buttonref.current.disabled = true
-            if(e.target.value){
-                e.target.value="";
+            if (e.target.value) {
+                e.target.value = "";
                 return;
             }
             let prev = e.target.previousSibling
-            if(prev){
+            if (prev) {
                 e.target.disabled = true
                 prev.disabled = false
                 prev.focus()
             }
-        }else if(e.key == "Enter" && buttonref.current.disabled == false){
+        } else if (e.key == "Enter" && buttonref.current.disabled == false) {
             submit()
         }
     }
-    return(
+    return (
         <div className="dinput" >
-            {[1,2,3,4].map((ind)=>{
-                return(
-                    <input key={ind} type="number" ref={(ele)=> (inputref.current[ind] = ele)} name={`i${ind}`} onInput={inp} onKeyDown={kdown} disabled={ind>1} onPaste={paste}/>
+            {[1, 2, 3, 4].map((ind) => {
+                return (
+                    <input key={ind} type="number" ref={(ele) => (inputref.current[ind] = ele)} name={`i${ind}`} onInput={inp} onKeyDown={kdown} disabled={ind > 1} onPaste={paste} />
                 )
             })}
         </div>
     )
-}
+})
 
 export default Cinput
--- src/components/download/download.jsx
+++ src/components/download/download.jsx
@@ -1,86 +1,19 @@
 import "./download.css"
 import Cinput from "./cinput";
 import Status from "../status/status";
-import { useEffect, useRef, useState } from "react";
-import { AES } from "crypto-js";
-import Toast from "../../helpers/toast";
-
+import { useCallback, useEffect, useRef, useState } from "react";
+import Downloadfunc from "../../hooks/downloadfunc";
 function Download(){
     const buttonRef = useRef(null)
     const inputRefs = useRef([])
     const [dper,setdper] = useState("0%")
     const [dfile,setdfile] = useState(false)
-    const download = ()=>{
-        let code = ""
-        inputRefs.current.forEach((input)=>{
-            code += input.value
-        })
-        let exp = /^[0-9]{4}$/;
-        if(!exp.test(code)){
-            Toast("Invalid Code!", "warn", localStorage.getItem("theme") || "dark")
-            return;
-        }
-        let data = JSON.stringify({id:code})
-        let token = AES.encrypt(data, import.meta.env.VITE_FILES_API_KEY).toString()
-        fetch("/api/files/download",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({token:token})
-        }).then((res)=>{
-            return res.json()
-        }).then((data)=>{
-            if(data.status){
-                setdfile(data)
-                inputRefs.current.forEach((input)=>{
-                    input.disabled = true 
-                })
-                buttonRef.current.disabled = true
-                let redirect = data.redirect
-                redirect = window.location.origin + "/api" + redirect
-                let xhr = new XMLHttpRequest()
-                xhr.open("GET", redirect)
-                xhr.responseType = "blob"
-                xhr.send()
-                xhr.onprogress = (e)=>{
-                    let per = parseInt(e.loaded/e.total * 100).toString()
-                    setdper(`${per}%`)
-                }
-                xhr.onload = ()=>{
-                    let headers = xhr.getResponseHeader("Content-Type")
-                    if(headers.includes("application/json")){
-                        Toast("Some Error Occured!!", "error", localStorage.getItem("theme") || "dark")
-                        return
-                    }
-                    let blob = xhr.response
-                    let url = URL.createObjectURL(blob)
-                    let a = document.createElement("a")
-                    a.href = url
-                    a.download = data.name
-                    a.click()
-                    Toast("File Downloaded Successfully", "success", localStorage.getItem("theme") || "dark")
-                    document.querySelectorAll(".download .dinput input").forEach((input)=>{
-                        input.value = ""
-                    })
-                    setTimeout(() => {
-                        setdper("0%")
-                        setdfile(false)
-                        inputRefs.current[1].disabled=false
-                        inputRefs.current[1].focus()
-                        buttonRef.current.disabled = false
-                    }, 3000);
-
-                }
-            }else{
-                Toast(data.message,"error",localStorage.getItem("theme") || "dark")
-            }
-        })
-    }
+    const download = useCallback(()=>Downloadfunc(inputRefs, buttonRef, setdper, setdfile),[])
     useEffect(()=>{
         buttonRef.current.addEventListener("click",download)
         inputRefs.current[1].focus()
     },[])
+    
     return(
         <div className="download">
             <h3>Download</h3>
